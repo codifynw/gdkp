56184158676fc7e7835af0dde00c23e50f70556d
Cody Wilson <cody@codifynw.com>
2021-03-27 12:38:21 -0700

    working chat / user connect

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ app.use(express.static("public"));
 // Socket setup
 const io = socket(server);
 
+const activeUsers = new Set();
+
 io.on("connection", function (socket) {
   console.log("Made socket connection");
+
+  socket.on("new user", function (data) {
+    socket.userId = data;
+    activeUsers.add(data);
+    io.emit("new user", [...activeUsers]);
+  });
+
+  socket.on("disconnect", () => {
+    console.log('user disconnected');
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+  socket.on("chat message", function (data) {
+    console.log('chat message received');
+    console.log(data);
+    io.emit("chat message", data);
+  });
 });
