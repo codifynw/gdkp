87ad029abdbb2959a65d8627385045642fc8340e
Cody Wilson <cody@codifynw.com>
2021-03-27 17:20:22 -0700

    working C,R in CRUD

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 // GET ALL
-router.get('/', (req, res) => {
-    res.send('Hello Worlddd')
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 // GET
@@ -13,8 +19,18 @@ router.get('/:id', (req, res) => {
 })
 
 // CREATE
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber ({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
 
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 // UPDATE
