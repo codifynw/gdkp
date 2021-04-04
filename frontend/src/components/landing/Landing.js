fd470be9066537e24ce8df7b782c86258c771cfc
Cody Wilson <cody@codifynw.com>
2021-04-04 01:34:32 -0700

    landing page

--- frontend/src/components/landing/Landing.js
+++ frontend/src/components/landing/Landing.js
@@ -4,10 +4,17 @@ import "./landing.css";
 class Landing extends Component {
   render() {
     return (
-      <div className="landing-container">
-        <span className="gold">SPICE UP YOUR RAID</span>
-        <span className="landing-title">GDKP PRO</span>
-        &mdash; <span className="gold">GET ALL THE STATS</span> &mdash;
+      <div>
+        <div className="landing-container">
+          <span className="gold">SPICE UP YOUR RAID</span>
+          <span className="landing-title">GDKP PRO</span>
+          &mdash; <span className="gold">GET ALL THE STATS</span> &mdash;
+        </div>
+        <div className="landing-options">
+          <div className="landing-option">START</div>
+          <div className="landing-option">JOIN</div>
+          <div className="landing-option">BROWSE</div>
+        </div>
       </div>
     );
   }
